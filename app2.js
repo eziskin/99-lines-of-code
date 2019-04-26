7564e304d9560e03cf991d3867e85c1497dfa9e7
eziskin <eliziskin@gmail.com>
2019-04-25 22:37:12 -0700

    finalized 99 lines of code

---
+++ app2.js
@@ -0,0 +1,11 @@
+var friends = ['Anton', 'Diana', 'John', 'Sara', 'Jovison'];
+
+for(var i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+
+    for(var n = 0; n < 99; n++) {
+        console.log(n);
+        console.log('How many roads must ' + (friends[i]) + ' walk down' + '?');
+    }
+
+}
